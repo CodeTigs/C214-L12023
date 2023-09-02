142f82689c0134715b567c0c25e6a5d370ccb011
Tiago Rodrigues <tiago.rodrigues@gec.inatel.br>
2023-09-01 23:09:57 -0300

    atualizacao do codigo

--- Aula3/app_imc/src/App.svelte
+++ Aula3/app_imc/src/App.svelte
@@ -1,3 +1,4 @@
+
 <script>
 	// Variáveis de estado
 	let peso = 0;      // Peso do usuário
@@ -12,6 +13,27 @@
 		imc = 0;
 	  }
 	}
+	// função para classificar o IMC
+	function classificarPeso(imc) {
+  if (imc < 18.5) {
+    return 'Abaixo do peso';
+  } else if (imc >= 18.5 && imc <= 24.9) {
+    return 'Peso ideal';
+  } else if (imc >= 25.0 && imc <= 29.9) {
+    return 'Levemente acima do peso';
+  } else if (imc >= 30.0 && imc <= 34.9) {
+    return 'Obesidade grau 1';
+  } else if (imc >= 35.0 && imc <= 39.9) {
+    return 'Obesidade grau 2 (severa)';
+  } else {
+    return 'Obesidade grau 3 (mórbida)';
+  }
+}
+	function exibirClassificacao(imc) {
+	  const classificacao = classificarPeso(imc);
+	  const resultadoElement = document.getElementById('resultado');
+	  resultadoElement.textContent = `Sua classificação de peso é: ${classificacao}`;
+	}
   </script>
   
   <main>
@@ -23,10 +45,13 @@
 	  Altura (m): <input type="number" bind:value={altura} on:input={calcularIMC} />
 	</label>
 	{#if imc > 0}
-	  <p>Seu IMC é {imc}</p>
+	  <script>
+		exibirClassificacao({imc});
+	  </script>
+	  <p>Sua classificacao peso e {classificarPeso(imc)}</p> <!-- Aqui vamos exibir a classificação -->
+
 	{/if}
   </main>
-  
   <style>
 	main {
 	  font-family: sans-serif;
